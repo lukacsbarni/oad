be71cc4b9c2762577b4d80aa1c5859663492d92e
Lukacs Barnabas <lukacsbarni@gmail.com>
2026-02-12 12:14:49 +0200

    duplicate provider

--- providers.tf
+++ providers.tf
@@ -1,9 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
