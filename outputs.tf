bbf67d8bfe8969d8963843c1211622a48a5bbddf
Lukacs Barnabas <lukacsbarni@gmail.com>
2026-02-12 12:35:10 +0200

    Outpus

--- outputs.tf
+++ outputs.tf
@@ -1,46 +1,14 @@
-data "azurerm_oracle_autonomous_database" "this" {
-  name                = azurerm_oracle_autonomous_database.this.name
-  resource_group_name = azurerm_oracle_autonomous_database.this.resource_group_name
-
-  depends_on = [azurerm_oracle_autonomous_database.this]
-}
-
 output "id" {
-  description = "The Azure resource ID of the Autonomous Database"
   value       = azurerm_oracle_autonomous_database.this.id
+  description = "Azure resource ID"
 }
 
 output "name" {
-  description = "The name of the Autonomous Database"
   value       = azurerm_oracle_autonomous_database.this.name
+  description = "Autonomous DB name"
 }
 
-output "display_name" {
-  description = "The display name of the Autonomous Database"
-  value       = azurerm_oracle_autonomous_database.this.display_name
-}
-
-output "lifecycle_state" {
-  description = "Current lifecycle state (from data source)"
-  value       = data.azurerm_oracle_autonomous_database.this.lifecycle_state
-}
-
-output "private_endpoint_ip" {
-  description = "Private endpoint IP address (from data source)"
-  value       = data.azurerm_oracle_autonomous_database.this.private_endpoint_ip
-}
-
-output "private_endpoint_label" {
-  description = "Private endpoint label (from data source)"
-  value       = data.azurerm_oracle_autonomous_database.this.private_endpoint_label
-}
-
-output "time_created" {
-  description = "Timestamp when the database was created (from data source)"
-  value       = data.azurerm_oracle_autonomous_database.this.time_created
-}
-
-output "nsg_id" {
-  description = "ID of the optional NSG created for allowed_ips (null if not created)"
-  value       = length(azurerm_network_security_group.adb) > 0 ? azurerm_network_security_group.adb[0].id : null
+output "resource_group_name" {
+  value       = azurerm_oracle_autonomous_database.this.resource_group_name
+  description = "Resource group name"
 }
